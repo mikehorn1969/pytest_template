26b7d803177b2fd202fb076d80e9a9897f4ef4e4
mikehorn1969 <mike@mikehorn.net>
2024-12-04 17:23:35 +0300

    first feature file and step defs

--- tests/features/bank_transactions.feature
+++ tests/features/bank_transactions.feature
@@ -2,9 +2,33 @@ Feature: Bank Transactions
 
     Tests related to banking Transactions
 
-    Scenario: Deposit into account
-        Given the account balance is £100
-        When I deposit £20
-        Then the account balance should be £120
+   """  Scenario: Deposit into account
+        Given the account balance is £"100"
+        When I deposit £"20"
+        Then the account balance should be £"120"
 
+    Scenario: Withdraw from account
+        Given the account balance is £"100"
+        When I withdraw £"20"
+        Then the account balance should be £"80" """
         
+    Scenario Outline: Deposit into account
+        Given the account balance - £<balance>
+        When I deposit £<deposit>
+        Then the account balance should be £<new_balance>
+        Examples:
+            | balance | deposit | new_balance |
+            | 100     | 20      | 120         |
+            | 0       | 5       | 5           |
+            | 100     | 0       | 100         |
+
+    Scenario Outline:  Withdraw from account
+        Given the account balance is £<balance>
+        When I withdraw £<withdrawal>
+        Then the account balance should be £<new_balance>
+        Examples
+            | balance | withdrawal | new_balance |
+            | 100     | 20         | 80          |
+            | 0       | 5          | -5          |
+            | 100     | 0          | 100         |
+
--- tests/step_definitions/test_bank_transactions.py
+++ tests/step_definitions/test_bank_transactions.py
@@ -1,5 +1,5 @@
 import pytest 
-from pytest_bdd import scenarios, scenario, given, when, then  
+from pytest_bdd import scenarios, scenario, given, when, then, parsers
   
 # Load all scenarios from the feature file  
 scenarios("../features/bank_transactions.feature")  
@@ -7,19 +7,25 @@ scenarios("../features/bank_transactions.feature")
 # Fixtures  
 @pytest.fixture  
 def account_balance():  
-    return {"balance": 100}  # Using a dictionary to allow modifications  
+    return {"balance": 0}  # Using a dictionary to allow modifications  
   
 # Given Steps  
-@given("the account balance is $100")  
-def account_initial_balance(account_balance):  
-    account_balance["balance"] = 100  
+@given(parsers.parse('the account balance is £"{balance:d}"'), target_fixture="account_balance",)
+def account_initial_balance(account_balance, balance):  
+    account_balance["balance"] = balance  
   
 # When Steps  
-@when("I deposit $20")  
-def deposit(account_balance):  
-    account_balance["balance"] += 20  
+@when(parsers.parse('I deposit £"{deposit:d}"')) 
+def deposit(account_balance, deposit):  
+    account_balance["balance"] += deposit  
+  
+# When Steps  
+@when(parsers.parse('I withdraw £"{withdrawal:d}"')) 
+def deposit(account_balance, withdrawal):  
+    account_balance["balance"] -= withdrawal  
+
   
 # Then Steps  
-@then("the account balance should be $120")  
-def account_balance_should_be(account_balance):  
-    assert account_balance["balance"] == 120
+@then(parsers.parse('the account balance should be £"{new_balance:d}"'))
+def account_balance_should_be(account_balance, new_balance):  
+    assert account_balance["balance"] == new_balance
